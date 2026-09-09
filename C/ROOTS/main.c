87eacdbe7c8f1bb3749c9cc34de25efc5259754b
Serhii Rusakov <sergej.rusakov.m@gmail.com>
2026-09-09 19:29:57 +0200

    changed proximity logic, added comments and minor redability improvements

--- C/ROOTS/main.c
+++ C/ROOTS/main.c
@@ -1,3 +1,9 @@
+/*
+Creates pseudo-random matrix with two biases based on threshold.
+Using stb_image creates roots.png with 3 colours.
+White for the "background", black for the "root" and grey for "branches"
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,25 +16,26 @@
 #define CORNER_BIAS -0.2 //being in a corner
 
 void generateMatrix(int rows, int cols, int rootRow, int rootCol, float threshold, float matrix[rows][cols]);
-int* twoDtoOneD(int rows, int cols, float matrix[rows][cols]);
+int degreeOfProximity (int rootRow, int rootCol, int currRow, int currCol);
+int * twoDtoOneD(int rows, int cols, float matrix[rows][cols]);
 void createPNG(int *array, int rows, int cols);
 
 int main()
 {
     srand(time(NULL));
 
-    int rows = 32;
-    int cols = 16;
+    const int rows = 16;
+    const int cols = 16;
     float matrix[rows][cols];
 
-    int rootRow = rand() % rows;
-    int rootCol = rand() % cols;
+    const int rootRow = rand() % rows;
+    const int rootCol = rand() % cols;
 
-    float threshold = .53;
+    const float threshold = .53;
 
     generateMatrix(rows, cols, rootRow, rootCol, threshold, matrix);
 
-    int *array = twoDtoOneD(rows, cols, matrix);
+    int * array = twoDtoOneD(rows, cols, matrix);
 
     createPNG(array, rows, cols);
 
@@ -37,25 +44,31 @@ int main()
 
 void generateMatrix(int rows, int cols, int rootRow, int rootCol, float threshold, float matrix[rows][cols])
 {
+    /*
+    Generates matrix based on biases
+    
+    rows, cols - dimensions of the matrix
+    rootRow, rootCol - parameters of the root
+    threshold - given threshold
+    matrix[rows][cols] - matrix to generate
+    */
     for (size_t i = 0; i < rows; i++)
     {
         for (size_t j = 0; j < cols; j++) {
+
+            if (i == rootRow && j == rootCol)
+            {
+                continue;
+            }
+            
             matrix[i][j] = (float)rand() / (float)RAND_MAX;
+            matrix[i][j] += (PROXIMITY_BIAS / degreeOfProximity(rootRow, rootCol, i, j));
 
             if ((i == 0 && j == 0) || (i == 0 && j == cols - 1) ||
                 (i == rows - 1 && j == 0) || (i == rows - 1 && j == cols - 1))
             {
                 matrix[i][j] += CORNER_BIAS;
-            }
-            if ((i == rootRow && abs(rootCol - j) == 1) ||
-                (j == rootCol && abs(rootRow - i) == 1))
-            {
-                matrix[i][j] += PROXIMITY_BIAS;
-            }
-            else if (abs(rootRow - i) == 1 && abs(rootCol - j) == 1)
-            {
-                matrix[i][j] += PROXIMITY_BIAS / 2;
-            }            
+            }         
             
             if (matrix[i][j] >= threshold)
             {
@@ -78,9 +91,28 @@ void generateMatrix(int rows, int cols, int rootRow, int rootCol, float threshol
     }
 }
 
-int* twoDtoOneD(int rows, int cols, float matrix[rows][cols])
+int degreeOfProximity (int rootRow, int rootCol, int currRow, int currCol)
 {
-    int *array = malloc(sizeof(int) * rows * cols);
+    /*
+    Returns non-euclidean distance from root to current pixel
+
+    rootRow, rootCol - parameters of the root
+    currRow, currCol - parameters of the current pixel
+    */
+    return abs(rootRow - currRow) + abs(rootCol - currCol);
+}
+
+int * twoDtoOneD(int rows, int cols, float matrix[rows][cols])
+{
+    /*
+    Convertes 2d matrix into 1d array
+    
+    rows, cols - dimensions of the matrix
+    matrix[rows][cols] - matrix to generate
+
+    returns pointer to a 1d array
+    */
+    int * array = malloc(sizeof(int) * rows * cols);
     if(array == NULL)
     {
         exit(-1);
@@ -97,12 +129,18 @@ int* twoDtoOneD(int rows, int cols, float matrix[rows][cols])
     return array;
 }
 
-void createPNG(int *array, int rows, int cols)
+void createPNG(int * array, int rows, int cols)
 {
+    /*
+    Creates 3-channel png using stbi_image_write
+
+    array - pointer to a 1d array
+    rows, cols - dimensions of the matrix
+    */
     int channels = 3;
     int size = rows * cols * channels;
 
-    unsigned char *data = malloc(size);
+    unsigned char * data = malloc(size);
     int i = 0;
     for (unsigned char *p = data; p != data + size; p+=channels)
     {
